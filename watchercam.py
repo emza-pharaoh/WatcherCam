34874f2f650fb55adce0b5bfd65bb8884120990c
Emanuel <emanuelmhlanga55@gmail.com>
2026-02-10 12:54:03 +0200

    functions!

--- watchercam.py
+++ watchercam.py
@@ -3,18 +3,35 @@ import numpy as np
 import time
 from telegram import Bot
 import os
-# ==================== CONFIG
-PHONE_NUMBER = "+27691163533"
-COOLDOWN = 15
-MOTION_FRAMES_REQUIRED = 10
+from dotenv import load_dotenv
 
-BOT_TOKEN = '8532265037:AAHfZf9IBLOGSqJNuMgG7UFMhJOlelWVjng'
-CHAT_ID = '6076583381'
+# ==================== CONFIG
+
+COOLDOWN = 15
+motion_history = []
+MOTION_FRAMES_REQUIRED = 4
+
+load_dotenv()
+
+PHONE_NUMBER = os.getenv("PHONE_NUMBER")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not loaded")
+
 bot = Bot(token=BOT_TOKEN)
 
 
 
-cap = cv2.VideoCapture(1) #Opening default camera
+cap = cv2.VideoCapture(0) #Opening default camera
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 460)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+
+
 
 bg_subtractor = cv2.createBackgroundSubtractorMOG2(
     history=500,
@@ -24,13 +41,19 @@ bg_subtractor = cv2.createBackgroundSubtractorMOG2(
 
 motion_counter = 0
 last_alert_time = 0
-
+kernel = np.array([[0, -1, 0],
+                   [-1, 5, -1],
+                   [0, -1, 0]])
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+    frame = cv2.filter2D(frame, -1, kernel)
 
-    fg_mask = bg_subtractor.apply(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    fg_mask = bg_subtractor.apply(gray)
 
     #clean mask
     fg_mask = cv2.GaussianBlur(fg_mask, (5, 5), 0)
@@ -52,18 +75,21 @@ while cap.isOpened():
     for contour in contours:
 
         #ignore small movements
-        if cv2.contourArea(contour) < 15000:
+        min_area = frame.shape[0] * frame.shape[1] * 0.02
+        if cv2.contourArea(contour) < min_area:
             continue
 
         x, y, w, h = cv2.boundingRect(contour)
         boxes.append((x, y, w, h))
 
 
-        #motion persistance
-    if boxes:
-        motion_counter += 1
-    else:
-        motion_counter = 0
+    #motion persistance
+
+    motion_history.append(1 if boxes else 0)
+    if len(motion_history) > MOTION_FRAMES_REQUIRED:
+        motion_history.pop(0)
+
+    motion_counter = sum(motion_history)
 
     # Draw boxes
     for (x, y, w, h) in boxes:
